018f8f6f770a670c7b30d3b9529977d165d44f35
Zhivotenko Oleksandr <zhivotenko.a.s@gmail.com>
2021-03-10 18:45:28 +0200

    homework #3

---
+++ model/schemas/contact.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+const { Schema,model } = mongoose
+ 
+const contactSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Set name for contact'],
+        unique:true,
+    },
+    email: {
+        type: String,
+        required: [true, 'Set email for contact'],
+        unique:true,
+    },
+    phone: {
+        type: Number,
+        required: [true,'Set phone for contact'],
+        min: 5,
+        
+    },
+    owner: {
+        name: String,
+        age: Number,
+        address:String
+    },
+
+},
+    {
+        versionKey: false,
+        timestamps:true
+    }
+);
+ 
+const Contact = model('contact', contactSchema)
+module.exports=Contact
